fa66c5384b17c1bc43a1c1ec26504ad223b11256
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-09-22 19:53:30 +0900

    [Update] 레트로핏 작업중

--- app/src/main/java/com/example/gsmgosu/WriteLayout.kt
+++ app/src/main/java/com/example/gsmgosu/WriteLayout.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gsmgosu.retrofit.RetrofitService
 import com.example.gsmgosu.retrofit.data.board.Board
+import kotlinx.android.synthetic.main.activity_write_layout.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,15 +14,19 @@ class WriteLayout : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_layout)
 
-        RetrofitService().getBoardAPI().postBoard().enqueue(object : Callback<Board>{
-            override fun onResponse(call: Call<Board>, response: Response<Board>) {
+        summit.setOnClickListener {
+            val writeTitle = write_title.text.toString()
+            val boardContent = board_content.text.toString()
+            RetrofitService().getBoardAPI().postBoard().enqueue(object : Callback<Board>{
+                override fun onResponse(call: Call<Board>, response: Response<Board>) {
 
-            }
+                }
 
-            override fun onFailure(call: Call<Board>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
+                override fun onFailure(call: Call<Board>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
 
-        })
+            })
+        }
     }
 }
