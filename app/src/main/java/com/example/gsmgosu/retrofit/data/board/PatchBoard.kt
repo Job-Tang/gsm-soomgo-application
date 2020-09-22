ea0f75b138b8055765510eb437cd4ff81069e8a6
structsize <52183019+structsize@users.noreply.github.com>
2020-09-22 17:57:32 +0900

    [UPDATE] update field to body

---
+++ app/src/main/java/com/example/gsmgosu/retrofit/data/board/PatchBoard.kt
@@ -0,0 +1,8 @@
+package com.example.gsmgosu.retrofit.data.board
+
+data class PatchBoard(
+    val grade : Int,
+    val postTitle : String,
+    val postContent : String,
+    val publisher : String
+)
