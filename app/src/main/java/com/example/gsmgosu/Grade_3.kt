ffd17b7cf9829acbc9f67738a0e57791b24e0856
structsize <52183019+structsize@users.noreply.github.com>
2020-09-22 11:27:45 +0900

    [UPDATE] update fragment and register Matser

--- app/src/main/java/com/example/gsmgosu/Grade_3.kt
+++ app/src/main/java/com/example/gsmgosu/Grade_3.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_grade_3.view.*
 
 class Grade_3 : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    lateinit var student : ArrayList<Student>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_grade_3, container, false)
+
+        view.third_grade.adapter = MyCustomAdapter(activity!!.applicationContext,student)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grade_3, container, false)
+        return view
     }
 
 }
